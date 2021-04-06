f27adb99ba3cf8d56a394b67b861f87a0c53e22f
Miryam Bathilde <mbathilde@gmail.com>
2021-04-06 15:29:00 +0200

    creamos bbdd

--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+#se crea una clase por cada tabla que necesites que tenga tu BBDD
+
+class Clientes(models.Model):
+    nombre = models.CharField(max_length=30)
+    direccion = models.CharField(max_length=50)
+    email = models.EmailField()
+    tfno = models.CharField(max_length=9)
+
+class Articulos(models.Model):
+    nombre = models.CharField(max_length=30)
+    seccion = models.CharField(max_length=20)
+    precio = models.IntegerField()
+
+class Pedidos(models.Model):
+    numero = models.IntegerField()
+    fecha = models.DateField()
+    entregado = models.BooleanField()
