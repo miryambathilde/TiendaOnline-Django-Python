583c01bebf0efd496bb0c684cbf264d5bbcd4063
Miryam Bathilde <mbathilde@gmail.com>
2021-04-07 15:04:29 +0200

    introduzco string en tabla articulos + filter wher

--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -14,6 +14,9 @@ class Articulos(models.Model):
     seccion = models.CharField(max_length=20)
     precio = models.IntegerField()
 
+    def __str__(self):
+        return 'El nombre es %s la seccion es %s y el precio es %s' % (self.nombre, self.seccion, self.precio)
+
 class Pedidos(models.Model):
     numero = models.IntegerField()
     fecha = models.DateField()
