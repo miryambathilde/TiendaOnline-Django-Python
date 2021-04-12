ea9cc9fffcbafb6475ee69f31b4dc8bb71ea53b5
Miryam Bathilde <mbathilde@gmail.com>
2021-04-12 14:10:17 +0200

    admin.ModelAdmin para ClientesAdmin

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 from gestionPedidos.models import Clientes, Articulos, Pedidos
 
 # Register your models here.
+class ClientesAdmin(admin.ModelAdmin):
+    list_display=("nombre", "direccion", "tfno")
+        
 
 #linea de codigo para tener desde el admin disponible nuestras tablas - entre parentesis el nombre del modelo
-admin.site.register(Clientes)
+admin.site.register(Clientes, ClientesAdmin)
 admin.site.register(Articulos)
 admin.site.register(Pedidos)
