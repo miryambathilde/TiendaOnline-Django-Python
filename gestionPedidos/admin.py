f4e02c04bfbbc5b27572d1fd6d31ff1d00d78bfe
Miryam Bathilde <mbathilde@gmail.com>
2021-04-12 13:41:53 +0200

    Manipulacion de tablas desde Panel Admin

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+#importacion de tabla clientes desde el archivo gestionPedidos-models.py
+from gestionPedidos.models import Clientes, Articulos, Pedidos
 
 # Register your models here.
+
+#linea de codigo para tener desde el admin disponible nuestras tablas - entre parentesis el nombre del modelo
+admin.site.register(Clientes)
+admin.site.register(Articulos)
+admin.site.register(Pedidos)
