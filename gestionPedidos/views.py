b9b6c21cdfbe7826114b7c3c155dbc1190192d43
Miryam Bathilde <mbathilde@gmail.com>
2021-04-14 13:43:08 +0200

    creamos consulta a sql producto + render html busq

--- gestionPedidos/views.py
+++ gestionPedidos/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 # importamos HttpResponse
 from django.http import HttpResponse
+from gestionPedidos.models import Articulos
 
 # Create your views here.
 def busqueda_productos(request):
@@ -9,7 +10,13 @@ def busqueda_productos(request):
 # %r es el request en este caso de buscar producto
 def buscar(request):
     if request.GET["prd"]:
-        mensaje="Artículo buscado: %r" %request.GET["prd"]
+        #mensaje="Artículo buscado: %r" %request.GET["prd"]
+        #variable producto para consulta PRD QUE ES PRODUCTO
+        producto=request.GET["prd"]
+        #icontains funciona como el like dentro de una consulta de una instruccion SQL, nos devuelve cualquier valor que contenga el nombre del producto sea en la posición que sea
+        articulos=Articulos.objects.filter(nombre__icontains=producto)
+        #redigir al html que muestre la información del producto buscado
+        return render(request, "resultado_busqueda.html", {"articulos": articulos, "query":producto})
         
     else:
         mensaje="No has introducido ningún producto"
